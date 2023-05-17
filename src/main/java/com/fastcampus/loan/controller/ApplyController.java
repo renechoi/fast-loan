43af1cee4760e8174139ec1c616cc6bd5c601199
renechoi <115696395+renechoi@users.noreply.github.com>
2023-05-17 20:59:13 +0900

    feat: implement file upload and download

--- src/main/java/com/fastcampus/loan/controller/ApplyController.java
+++ src/main/java/com/fastcampus/loan/controller/ApplyController.java
@@ -2,11 +2,22 @@ package com.fastcampus.loan.controller;
 
 import static com.fastcampus.loan.dto.ApplyDTO.*;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
 import com.fastcampus.loan.dto.ApplyDTO;
+import com.fastcampus.loan.dto.FileDTO;
 import com.fastcampus.loan.dto.ResponseDTO;
 import com.fastcampus.loan.service.ApplicationService;
+import com.fastcampus.loan.service.FileStorageService;
+
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +25,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 
 @RequiredArgsConstructor
 @RestController
@@ -22,6 +36,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApplyController extends AbstractController {
 
   private final ApplicationService applicationService;
+  private final FileStorageService fileStorageService;
 
   @PostMapping
   public ResponseDTO<Response> create(@RequestBody Request request) {
@@ -48,5 +63,37 @@ public class ApplyController extends AbstractController {
   @PostMapping("/{applicationId}/terms")
   public ResponseDTO<Boolean> acceptTerms(@PathVariable Long applicationId, @RequestBody AcceptTerms request) {
     return ok(applicationService.acceptTerms(applicationId, request));
+  }
+
+  @PostMapping(value = "/{applicationId}/files")
+  public ResponseDTO<Void> upload(@PathVariable Long applicationId, MultipartFile file) throws IllegalStateException,
+      IOException {
+    fileStorageService.save(applicationId, file);
+    return ok();
+  }
+
+  @GetMapping("/{applicationId}/files")
+  public ResponseEntity<Resource> download(@PathVariable Long applicationId, @RequestParam(value="filename") String filename) throws IllegalStateException, IOException {
+    Resource file = fileStorageService.load(applicationId, filename);
+    return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+        "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+  }
+
+  @DeleteMapping("/{applicationId}/files")
+  public ResponseDTO<Void> deleteAll(@PathVariable Long applicationId) {
+    fileStorageService.deleteAll(applicationId);
+    return ok();
+  }
+
+  @GetMapping("/{applicationId}/files/infos")
+  public ResponseDTO<List<FileDTO>> getFileInfos(@PathVariable Long applicationId) {
+    List<FileDTO> fileInfos = fileStorageService.loadAll(applicationId).map(path -> {
+      String fileName= path.getFileName().toString();
+      return FileDTO.builder()
+          .name(fileName)
+          .url(MvcUriComponentsBuilder.fromMethodName(ApplyController.class, "download", applicationId, fileName).build().toString()).build();
+    }).collect(Collectors.toList());
+
+    return ok(fileInfos);
   }
 }
